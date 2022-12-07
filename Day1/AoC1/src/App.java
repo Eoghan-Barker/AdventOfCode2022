43999f95fa82c2a11e5a21cb601faf6fec8cbb95
Eoghan-Barker <g00397072@gmit.ie>
2022-12-07 10:54:45 +0000

    Part 2 done

--- Day1/AoC1/src/App.java
+++ Day1/AoC1/src/App.java
@@ -9,30 +9,50 @@ public class App {
 
         // variables
         int cals = 0;
-        int mostCalories = 0;
         int counter = 0;
         String readIn;
-        
+        int[] top3 = { 0, 0, 0 };
+
         // Parse through file
-        while(reader.hasNextLine()){
+        while (reader.hasNextLine()) {
             readIn = reader.nextLine();
-            
+
             // if readIn is empty = \n which means move on to a new elf
-            if(readIn.isEmpty()){
-                // check if this elf's calories are higher than the current highest
-                if(counter > mostCalories){
-                    mostCalories = counter;
+            if (readIn.isEmpty()) {
+                if (counter > top3[0]) {
+                    if (top3[0] > top3[1]) {
+                        if (top3[1] > top3[2]) {
+                            top3[2] = top3[1];
+                        }
+                        top3[1] = top3[0];
+                    } else if (top3[0] > top3[2]) {
+                        top3[2] = top3[0];
+                    }
+                    top3[0] = counter;
+                } else if (counter > top3[1]) {
+                    if (top3[1] > top3[2]) {
+                        top3[2] = top3[1];
+                    }
+                    top3[1] = counter;
+                } else if (counter > top3[2]) {
+                    top3[2] = counter;
                 }
+
                 // reset on new line
                 counter = 0;
             }
             // add calories for this elf
-            else{
+            else {
                 cals = Integer.parseInt(readIn);
                 counter += cals;
             }
         }
-        // Output the number of calories the elf with the most calories has
-        System.out.println(mostCalories);
+
+        reader.close();
+
+        // // Output the number of calories the elf with the most calories has
+        System.out.println(top3[0]);
+        // Output the number of calories held by the top 3 elfs
+        System.out.println(top3[0] + top3[1] + top3[2]);
     }
 }
